20f2de70dcb5d48ded6ad7e7506fabc063ace543
qinqinzoubaobao <trs_zs@163.com>
2019-02-10 18:59:56 +0800

    3

---
+++ src/main/java/com/zs/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.zs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String Hello() {
+		return "hello";
+	}
+	
+}
