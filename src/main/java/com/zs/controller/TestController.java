c06bfaeb524753a02c58e1e8859cc665e9b7bb21
qinqinzoubaobao <trs_zs@163.com>
2019-02-15 15:57:55 +0800

    4

--- src/main/java/com/zs/controller/TestController.java
+++ src/main/java/com/zs/controller/TestController.java
@@ -3,15 +3,16 @@ package com.zs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/test")
+/*@RequestMapping("/test")*/
 public class TestController {
 
 	@RequestMapping("/hello")
-	@ResponseBody
-	public String Hello() {
-		return "zou hello";
+/*	@ResponseBody*/
+	public ModelAndView  Hello() {
+		return new ModelAndView("top");
 	}
 	
 }
